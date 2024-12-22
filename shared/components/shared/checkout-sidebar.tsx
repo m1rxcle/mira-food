9140f53fbfaee119ba2c8a3a00b61b0c8992c3ca
Denis M1rxcle <noonebesideu@gmail.com>
2024-12-23 00:11:12 +0200

    Адаптация под телефон №2

--- shared/components/shared/checkout-sidebar.tsx
+++ shared/components/shared/checkout-sidebar.tsx
@@ -18,7 +18,7 @@ export const CheckoutSidebar: React.FC<Props> = ({ loading, totalAmount, classNa
 		<WhiteBlock className={cn("p-6 sticky top-4", className)}>
 			<div className="flex flex-col gap-1">
 				<span className="text-xl">Итого:</span>
-				<span className="text-[34px] font-extrabold">{loading ? <Skeleton className="h-[51px] w-[150px]" /> : totalPrice + "₽"}</span>
+				<span className="text-[34px] font-extrabold">{loading ? <Skeleton className="h-[51px] w-[150px]" /> : totalPrice + " ₽"}</span>
 			</div>
 
 			<CheckoutItemDetails
